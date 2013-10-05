a11344ce82deaf520f40fdf7c879fee8db6c833d
Egor Lynko <flexoid@gmail.com>
2013-10-05 14:00:08 +0300

    Check for current user

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,6 @@ class MoviesController < ApplicationController
   def show
     @actors = @movie.actors.includes(:movie_people)
     @reviews = @movie.reviews.approved
-    @grade = current_user.grade_for_movie(@movie)
+    @grade = current_user.grade_for_movie(@movie) if current_user
   end
 end
